5eb5a6a2c0154ef939daea1c0274c66b83886e36
siddhi <siddhi@gmail.com>
2026-02-01 23:55:32 +0530

    completed till day30

--- main.py
+++ main.py
@@ -120,4 +120,236 @@
 #         print("Adult")
 # You can add an if statement to a case for even more control. 
 # This is called a Guard.
+ 
+# day17
+# For Loops in Python
 
+# names = ["siddhi", "nisha", "serena"]
+# for name in names:
+#     print(name)
+# for i in range(5):
+#     print(f"Iteration number: {i}")
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+# for number in numbers:
+#     if number == 3:
+#         continue
+#     if number == 8:
+#         break
+#     print(number)
+
+# raw_names = [" siddhi", "yash ", "  nisha"]
+
+# for name in raw_names:
+#     clean_name = name.strip().capitalize()
+#     print(f"Hello, {clean_name}!")
+
+# Day18
+# Python While Loops
+
+# Python has two primitive loop commands:
+
+# while loops
+# for loops
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
+# i = 1
+# while i < 8:
+#     print(i)
+#     if i == 5:
+#         break
+#     i+=1
+# i = 1
+# while i < 8:
+#         i+=1
+#         if i == 5:
+#             continue
+#         print(i)
+
+# day19
+# break,pass and continue in Python
+# In Python, break, continue, and pass 
+# are used to control the flow of loops and handle empty code blocks.
+# for i in range(1, 10):
+#     if i == 5:
+#         break  # Stops the loop when i is 5
+#      print(i)
+# for i in range(1, 6):
+#     if i == 3:
+#         continue  # Skips printing 3
+#     print(i)
+# if True:
+#     pass  # I will add logic here later!
+
+# print("Code runs because pass filled the empty block")
+# break	Stops the loop completely.	You found what you were looking for.
+# continue	Skips the rest of the current turn.	You want to ignore one specific item.
+# pass	Does nothing at all.	Placeholder to prevent errors
+
+# day20
+# Python Functions
+# A function is a block of code which only runs when it is called.
+# A function can return data as a result.
+# A function helps avoiding code repetition.
+# In Python, a function is defined using the def keyword, followed by a function name and parentheses
+# def my_function():
+#   print("Hello from a function")
+
+# my_function()
+# my_function()
+# my_function()
+
+# def fahrenheit_to_celsius(fahrenheit ):
+#     return (fahrenheit -32)*5/9
+
+# print(fahrenheit_to_celsius(50))
+# print(fahrenheit_to_celsius(89))
+
+# day21
+# Python Function Arguments
+# A parameter is the variable listed inside the parentheses in the function definition.
+# An argument is the actual value that is sent to the function when it is called.
+# def my_function(name): # name is a parameter
+#   print("Hello", name)
+
+# my_function("siddhi") # "siddhi" is an argument
+
+# If your function expects 2 arguments, you must call it with exactly 2 arguments.
+# If you try to call the function with the wrong number of arguments, you will get an error:
+
+# Default Parameter Values
+
+# def my_function(x="siddhi"):
+#     print ("hello" ,x)
+
+# my_function()
+# my_function("yash")
+
+# Keyword Arguments
+# You can send arguments with the key = value syntax.
+
+# def my_function(animal, name):
+#   print("I have a", animal)
+#   print("My", animal + "'s name is", name)
+
+# my_function(animal = "dog", name = "Buddy")
+
+# day 22
+# Python Lists
+
+# Lists are created using square brackets:
+# Lists are used to store multiple items in a single variable
+# Lists are one of 4 built-in data types in Python used to store collections
+# of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist) 
+
+# List Items
+# List items are ordered, changeable, and allow duplicate values.
+# List items are indexed, the first item has index [0], the second item has index [1] etc.
+
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+# A list with strings, integers and boolean values:
+
+# list1 = ["abc", 34, True, 40, "male"]
+# print(type(list1))
+
+# Python - Access List Items
+# link = "https://www.w3schools.com/python/python_lists_access.asp"
+
+# List Methods
+# Method - Description
+# append() - Adds an element at the end of the list
+# clear() - Removes all the elements from the list
+# copy() - Returns a copy of the list
+# count() - Returns the number of elements with the specified value
+# extend() - Add the elements of a list (or any iterable), to the end of the current list
+# index() - Returns the index of the first element with the specified value
+# insert() - Adds an element at the specified position
+# pop()	- Removes the element at the specified position
+# remove()	- Removes the item with the specified value
+# reverse()	- Reverses the order of the list
+# sort() - Sorts the list
+
+# link = https://www.w3schools.com/python/python_lists_methods.asp
+
+# day24
+# Python Tuples
+# Tuples are used to store multiple items in a single variable.
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+# A tuple is a collection which is ordered and unchangeable.
+# Tuples are written with round brackets.
+
+# mytuple = ("apple", "banana", "cherry")
+# print(mytuple)
+ 
+# day25
+# Python - Tuple Methods
+# count() - Returns the number of times a specified value occurs in a tuple
+# index() - Searches the tuple for a specified value and returns the position of where it was found
+ 
+# day 28
+# f-string
+# txt = f"The price is {20 * 59} dollars"
+# print(txt)
+
+# price = 59
+# print(f"The price is {price:.2f} dollars")
+
+# day29
+# Docstring
+# In Python, a Docstring (short for documentation string) is a special type of string used to explain what a specific module, function, class, or method does. Unlike regular comments (#), docstrings are actually stored as part of the code's metadata, making them accessible while the program is running.
+
+# def greet(name):
+#     """This function greets the person passed in as a parameter."""
+#     print(f"Hello, {name}!")
+
+# greet("siddhi")    
+
+# How to Access Docstrings
+# print(greet.__doc__)
+# help(greet)
+
+# day 30
+# Recursion in Python
+# Recursion is when a function calls itself.
+
+# Recursion is a common mathematical and programming concept. 
+# It means that a function calls itself. 
+# This has the benefit of meaning that you can loop through data to reach a result.
+
+# The developer should be very careful with recursion as it can be quite 
+# easy to slip into writing a function which never terminates, or one that 
+# uses excess amounts of memory or processor power. 
+# However, when written correctly recursion can be a very efficient and 
+# mathematically-elegant approach to programming.
+
+# def countdown(n):
+#   if n <= 0:
+#     print("Done!")
+#   else:
+#     print(n)
+#     countdown(n - 1)
+
+# countdown(5)
+
+# Base Case and Recursive Case
+# Every recursive function must have two parts:
+# A base case - A condition that stops the recursion
+# A recursive case - The function calling itself with a modified argument
+
+# def factorial(n):
+#   # Base case
+#   if n == 0 or n == 1:
+#     return 1
+#   # Recursive case
+#   else:
+#     return n * factorial(n - 1)
+
+# print(factorial(8))
+
+# import sys
+# sys.setrecursionlimit(2000)
+# print(sys.getrecursionlimit())
